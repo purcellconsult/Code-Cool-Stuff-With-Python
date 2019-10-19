a96b73b5e969dc12c43a2037375e39d30de731a2
purcellconsult <purcellconsult@gmail.com>
2019-10-18 22:22:50 -0700

    Add files via upload
    square artwork code.

---
+++ sourcecode/ch_04/square_artwork.py
@@ -0,0 +1,14 @@
+import turtle
+from random_colors import get_random_color
+square = turtle.Turtle()
+square.hideturtle()
+square.speed(0)
+for x in range(50):
+    square.color(get_random_color(), get_random_color())
+    square.begin_fill()
+    square.right(35)
+    for y in range(4):
+        square.forward(100)
+        square.right(90)
+    square.end_fill()
+turtle.done()
