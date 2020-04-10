36ff6e451aa637ad53e483956498c307694448ad
purcellconsult <purcellconsult@gmail.com>
2020-04-09 19:33:06 -0700

    Add files via upload
    Code snippet I've just cooked up.

---
+++ sourcecode/ch_03/draw_unique_nums.py
@@ -0,0 +1,35 @@
+"""
+A very non-efficient way to randomly draw numbers
+and print unique output each time.  
+"""
+
+from random import choice
+
+
+def straight_bet(games=3, nums=9):
+    cycles = games
+    pass_nums = []
+    roll = [x for x in range(nums + 1)]
+    while cycles > 0:
+        rand_one = choice(roll)
+        rand_two = choice(roll)
+        rand_three = choice(roll)
+        if rand_one in pass_nums or rand_one == rand_two or rand_one == rand_three:
+            continue
+        elif rand_two in pass_nums or rand_two == rand_three:
+            continue
+        elif rand_three in pass_nums:
+            continue
+        else:
+            cycles -= 1
+            print('{} {} {}'.format(rand_one, rand_two, rand_three))
+            pass_nums.append(rand_one)
+            pass_nums.append(rand_two)
+            pass_nums.append(rand_three)
+            roll.remove(rand_one)
+            roll.remove(rand_two)
+            roll.remove(rand_three)
+            print(cycles)
+
+
+straight_bet()
